30d34346b074253b27b9452c265b86e35fbf9a04
Kimnamgyu <andynamgoo@gmail.com>
2026-05-29 15:48:40 +0900

    add url

--- workspace/codingtest/KthNumber.java
+++ workspace/codingtest/KthNumber.java
@@ -2,6 +2,7 @@ package codingtest;
 
 public class KthNumber {
     public static void main(String[] args) {
+        // https://school.programmers.co.kr/learn/courses/30/lessons/42748
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
--- workspace/codingtest/PrimeNumber.java
+++ workspace/codingtest/PrimeNumber.java
@@ -2,6 +2,7 @@ package codingtest;
 
 public class PrimeNumber {
     public static void main(String[] args) {
+        // https://school.programmers.co.kr/learn/courses/30/lessons/12921
         int n = 2;
         int answer = 0;
         for(int i = 2; i <= n; i++) {
