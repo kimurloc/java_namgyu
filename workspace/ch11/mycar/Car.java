6c8cfbf4d719ccb8255def0e6bbd6b696b78a8a6
Kimnamgyu <andynamgoo@gmail.com>
2026-06-01 15:55:16 +0900

    add ch11 package

---
+++ workspace/ch11/mycar/Car.java
@@ -0,0 +1,42 @@
+package ch11.mycar;
+
+public class Car {
+    String model;
+    int speed;
+    int gear;
+
+    void speedUp() {
+        speed += 10;
+        System.out.println("accelerating! speed now : " + speed + "km/h");
+    }
+
+    void speedDown() {
+        speed -= 10;
+        System.out.println("decelerating.. speed now : " + speed + "km/h");
+    }
+
+    void gearUp() {
+        gear++;
+        System.out.println("1 gear up! gear now : " + gear);
+    }
+
+    void gearDown() {
+        gear--;
+        System.out.println("1 gear down.. gear now : " + gear);
+    }
+
+    void start() {
+        System.out.println("start driving");
+        gearUp();
+        speedUp();
+    }
+
+    void stop() {
+        System.out.println("stop driving");
+        while(speed > 0) {
+            speedDown();
+        }
+        gear = 0;
+    }
+
+}
---
+++ workspace/ch11/mycar/CarTest.java
@@ -0,0 +1,13 @@
+package ch11.mycar;
+
+public class CarTest {
+    void main() {
+        Car car = new Car();
+        car.model = "ray";
+        car.speed = 30;
+        car.gear = 0;
+
+        car.start();
+        car.stop();
+    }
+}
